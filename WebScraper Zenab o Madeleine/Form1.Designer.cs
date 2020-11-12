7e8e6ead1bf5af161d73936f91d3d8df153f97ff
Zenab99 <51152564+Zenab99@users.noreply.github.com>
2020-11-12 21:32:30 +0100

    Add project files.

---
+++ WebScraper Zenab o Madeleine/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WebScraper_Zenab_o_Madeleine
+{
+    partial class Form1
+    {
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.URLInput = new System.Windows.Forms.TextBox();
+            this.buttonExtract = new System.Windows.Forms.Button();
+            this.labelNumberOfImages = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.buttonSave.Font = new System.Drawing.Font("Stencil", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.Location = new System.Drawing.Point(444, 561);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(223, 61);
+            this.buttonSave.TabIndex = 0;
+            this.buttonSave.Text = "Save Pic";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // URLInput
+            // 
+            this.URLInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.URLInput.Location = new System.Drawing.Point(203, 59);
+            this.URLInput.Margin = new System.Windows.Forms.Padding(4);
+            this.URLInput.Multiline = true;
+            this.URLInput.Name = "URLInput";
+            this.URLInput.Size = new System.Drawing.Size(707, 37);
+            this.URLInput.TabIndex = 1;
+            this.URLInput.TextChanged += new System.EventHandler(this.URLInput_TextChanged);
+            // 
+            // buttonExtract
+            // 
+            this.buttonExtract.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExtract.BackColor = System.Drawing.Color.Blue;
+            this.buttonExtract.Font = new System.Drawing.Font("Stencil", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExtract.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonExtract.Location = new System.Drawing.Point(489, 104);
+            this.buttonExtract.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonExtract.Name = "buttonExtract";
+            this.buttonExtract.Size = new System.Drawing.Size(152, 37);
+            this.buttonExtract.TabIndex = 2;
+            this.buttonExtract.Text = "Extract";
+            this.buttonExtract.UseVisualStyleBackColor = false;
+            this.buttonExtract.Click += new System.EventHandler(this.buttonExtract_Click);
+            // 
+            // labelNumberOfImages
+            // 
+            this.labelNumberOfImages.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelNumberOfImages.AutoSize = true;
+            this.labelNumberOfImages.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNumberOfImages.Location = new System.Drawing.Point(16, 595);
+            this.labelNumberOfImages.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelNumberOfImages.Name = "labelNumberOfImages";
+            this.labelNumberOfImages.Size = new System.Drawing.Size(0, 22);
+            this.labelNumberOfImages.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Navy;
+            this.label1.Font = new System.Drawing.Font("Stencil", 13.8F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(373, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "VV Paste Your URL Here VV ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "%userprofile%\\Desktop";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(188, 148);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(754, 388);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1159, 635);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelNumberOfImages);
+            this.Controls.Add(this.buttonExtract);
+            this.Controls.Add(this.URLInput);
+            this.Controls.Add(this.buttonSave);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Image";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox URLInput;
+        private System.Windows.Forms.Button buttonExtract;
+        private System.Windows.Forms.Label labelNumberOfImages;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ WebScraper Zenab o Madeleine/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace WebScraper_Zenab_o_Madeleine
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        private HttpClient client = new HttpClient();
+        private Stopwatch restart = new Stopwatch();
+        private Dictionary<Task<byte[]>, string> allImageBytes = new Dictionary<Task<byte[]>, string>();
+        
+
+
+
+        private async void buttonExtract_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            string pattern = @"(?<=<img.*src="")[^""]+(?="".*)";
+            string input = URLInput.Text;
+
+            if (!input.StartsWith("http"))
+            {
+               
+                input = $"http://{input}";
+            }
+
+
+            string website = await client.GetStringAsync(input);
+            MatchCollection matches = Regex.Matches(website, pattern, RegexOptions.IgnoreCase);
+
+            foreach (Match match in matches)
+            {
+                if (!match.Value.StartsWith("http", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    listBox1.Items.Add(input + match.Value);
+                }
+                else
+                {
+                    listBox1.Items.Add(match.Value);
+                }
+            }
+
+            if (listBox1.Items.Count != 0)
+            {
+                buttonSave.Enabled = true;
+              
+                labelNumberOfImages.Text = $"Found {listBox1.Items.Count} images.";
+            }
+        }
+
+
+        private async void buttonSave_Click(object sender, EventArgs e)
+        {
+                       
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+               string directory = folderBrowserDialog1.SelectedPath;
+                Directory.CreateDirectory(directory);
+
+                await download(directory);
+            }        
+
+        }
+
+        private int map;
+
+        private async Task download(string directory)
+        {
+          
+
+            foreach (string same in listBox1.Items)
+            {
+              
+                Task<byte[]> imageTask = client.GetByteArrayAsync(same);
+
+                allImageBytes.Add(imageTask, same);
+            }
+
+
+            while (allImageBytes.Count > 0)
+            {
+                Task<byte[]> completedTask = null;
+
+                map = 0;
+                
+                    completedTask = await Task.WhenAny(allImageBytes.Keys);
+                
+                    Uri fileURL = new Uri(allImageBytes[completedTask]);
+
+                    using (FileStream fileStream = File.Open(Path.Combine(directory, map + Path.GetFileName(fileURL.AbsolutePath)), FileMode.Create))
+                    {
+                        await fileStream.WriteAsync(completedTask.Result, 0, completedTask.Result.Length);                       
+                      
+                    }  
+
+
+                    allImageBytes.Remove(completedTask);
+                
+                
+                    if (completedTask != null)
+                    {
+                        allImageBytes.Remove(completedTask);
+                    }
+                
+            }
+        }
+       
+       
+
+       
+        private void URLInput_TextChanged(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
